9dc30ce414e97b8c14e9fa5777511a306fda1543
Miguel-Oseguera <ml.oseguera@yahoo.com>
2023-10-23 14:58:43 -0500

    start

---
+++ tese.py
@@ -0,0 +1,11 @@
+userDictionary = {}
+print("set up dictionary")
+while True:
+    print("Name of products? (if no more products, input -1)")
+    userInfo = input()
+    if userInfo == -1:
+        break
+    userDictionary[userInfo] = []
+
+
+
